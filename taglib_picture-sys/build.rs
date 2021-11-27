732699986654fe3e0f0b5112b9ef2e101a2b62be
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2021-11-27 20:14:10 +0900

    add devcontainer and update to edition 2021

--- taglib_picture-sys/build.rs
+++ taglib_picture-sys/build.rs
@@ -1,27 +1,24 @@
 use std::path::PathBuf;
 
 fn main() {
-    let lib_res = pkg_config::probe_library("taglib");
-    if let Err(e) = lib_res {
-        println!("{}", e);
-    } else {
-        let lib = lib_res.unwrap();
+    let lib = pkg_config::probe_library("taglib").expect("Unable to find taglib.");
 
-        cc::Build::new()
-            .cpp(true)
-            .warnings(true)
-            .flag("-Wall")
-            .flag("-std=c++17")
-            .file("src/c/tag_c_picture.cpp")
-            .includes(lib.include_paths)
-            .include("src/c")
-            .compile("libtag_c_picture.a");
+    cc::Build::new()
+        .cpp(true)
+        .warnings(true)
+        .flag("-Wall")
+        .flag("-std=c++17")
+        .file("src/c/tag_c_picture.cpp")
+        .includes(lib.include_paths)
+        .include("src/c")
+        .compile("libtag_c_picture.a");
 
-        bindgen::Builder::default()
-            .header("src/c/tag_c_picture.h")
-            .generate()
-            .expect("unable to generate binding.")
-            .write_to_file(PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("binding.rs"))
-            .expect("couldn't write binding file.");
-    }
+    bindgen::Builder::default()
+        .header("src/c/tag_c_picture.h")
+        .generate()
+        .expect("unable to generate binding.")
+        .write_to_file(PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("binding.rs"))
+        .expect("couldn't write binding file.");
+    
+    println!("cargo:rerun-if-changed=src/c/tag_c_picture.cpp")
 }
